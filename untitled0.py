a94281acf81d9993a5df3b73d58ff41e1abdc4da
soumendas12 <102890752+soumendas12@users.noreply.github.com>
2024-06-22 14:47:02 +0530

    Add files via upload

---
+++ untitled0.py
@@ -0,0 +1,35 @@
+import tensorflow as tf
+from tensorflow.keras import layers, models
+
+# Define the generator model
+def make_generator_model():
+    model = tf.keras.Sequential()
+    model.add(layers.Dense(7*7*256, use_bias=False, input_shape=(100,)))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Reshape((7, 7, 256)))
+    assert model.output_shape == (None, 7, 7, 256) 
+
+    model.add(layers.Conv2DTranspose(128, (5, 5), strides=(1, 1), padding='same', use_bias=False))
+    assert model.output_shape == (None, 7, 7, 128)
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Conv2DTranspose(64, (5, 5), strides=(2, 2), padding='same', use_bias=False))
+    assert model.output_shape == (None, 14, 14, 64)
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Conv2DTranspose(1, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
+    assert model.output_shape == (None, 28, 28, 1)
+
+    return model
+
+generator = make_generator_model()
+
+# Generate a random noise vector
+noise = tf.random.normal([1, 100])
+
+# Generate an image
+generated_image = generator(noise, training=False)
